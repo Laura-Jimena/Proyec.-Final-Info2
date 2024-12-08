ee698b02d5be926f1c847007bfb5e7d9677b72a7
Laura-Jimena <laura.munevar@udea.edu.co>
2024-12-07 22:09:20 -0500

    Conexión de 3eras ventanas y configuración de selector de archivos

--- Vista.py
+++ Vista.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow, QDialog,QMessageBox
+from PyQt5.QtWidgets import QApplication,QMainWindow, QDialog,QMessageBox,QFileDialog
 from PyQt5.QtGui import QDoubleValidator, QRegExpValidator,QIntValidator
 from PyQt5.QtCore import Qt,QRegExp
 from PyQt5.uic import loadUi
@@ -17,8 +17,13 @@ class pprincipal(QMainWindow):
         loadUi('pprincipal.ui',self)
         self.setup()
     def setup(self):
+        # self.ingres_boton.clicked.connect(self.Verificar_cred)
         self.ingres_boton.clicked.connect(self.Abrir_menu)
-        self.bot_cuenta.clicked.connect(sel.Abrir_nuev_cuent)
+        self.bot_cuenta.clicked.connect(self.Abrir_nuev_cuenta)
+    # def Verificar_cred(self):
+    #     cusuario=self.usu_input.text()
+    #     ccontrasena=self.contra_input.text()
+    #     self._ventana_pprincipal.Recibir_cred(cusuario,ccontrasena)
     def Abrir_menu(self):
         ventana_menu=menu(self)
         self.hide()
@@ -27,7 +32,12 @@ class pprincipal(QMainWindow):
         ventana_cuenta=nueva_cuenta(self)
         self.hide()
         ventana_cuenta.show()
-#Ventana de creación de nueva cuenta.
+    def Recibir_nuev_cuenta(self, nomb,id,telef,email,usua,contras):
+        self.__mi_controlador.Recibir_nueva_cuenta(self, nomb,id,telef,email,usua,contras)
+    def Recibir_cred(self,usu,con):
+        self.__mi_controlador.Recibir_cred(usu,con)
+    def Asignar_cont(self,c):
+        self.__mi_controlador=c
 class nueva_cuenta(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -37,15 +47,31 @@ class nueva_cuenta(QMainWindow):
     def setup(self):
         self.nomb_input.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
         self.id_input.setValidator(QIntValidator())
-        self.telef_input.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
+        self.telef_input.setValidator(QIntValidator())
         self.usua_input.setValidator(QRegExpValidator(QRegExp("[a-zA-Z ]+")))
+        self.contra_input.setValidator(QRegExpValidator(QRegExp("[a-zA-Z0-9 ]+")))
+        self.acept_boton.clicked.connect(self.opc_aceptar)
+        self.volver_boton.clicked.connect(self.opc_volver)
+    def opc_aceptar(self):
+        nomb =  self.nomb_input.text()
+        id = self.id_input.text()
+        telef= self.telef_input.text() ### Consulatr metodo ver 
+        email= self.email_input.text()
+        usua = self.usua_input.text()
+        contras = self.contra_input.text()
+        self._ventana_pprincipal.Recibir_nuev_cuenta(nomb,id,telef,email,usua,contras)
+        self._ventana_pprincipal.show()
+        self.hide()
+    def opc_volver(self):
+        self.hide()
+        self._ventana_pprincipal.show()
 
 class menu(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         loadUi('Menu.ui',self)
         self._ventana_pprincipal=parent
-        # self.setup()
+        self.setup()
     def setup(self):
         self.rx_bot.clicked.connect(self.Abrir_rx)
         self.ecg_bot.clicked.connect(self.Abrir_ecg)
@@ -57,38 +83,84 @@ class menu(QMainWindow):
     def Abrir_ppg(self):
         ventana_ppg=ppg(self)
         self.hide()
-        ventana_ppg.show
+        ventana_ppg.show()
 
     def Abrir_ecg(self):
         ventana_ecg=ecg(self)
         self.hide()
-        ventana_ecg.show
+        ventana_ecg.show()
 
     def Abrir_rx(self):
         ventana_rx=rx(self)
         self.hide()
-        ventana_rx.show
+        ventana_rx.show()
 
 class rx(QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self,parent=None):
         super().__init__(parent)
         loadUi('rx.ui',self)
+        self._ventana_menu=parent
         self.setup()
     def setup(self):
+        self.volver_bot.clicked.connect(self.opcion_volver)
+        self.subir_arc.clicked.connect(self.Abrir_archivo)
+    def opcion_volver(self):
+        self.hide()
+        self._ventana_menu.show()
+    def Abrir_archivo(self):
+        archivo, _ = QFileDialog.getOpenFileName(self, "Abrir archivo", "", "Todos los archivos (*)")
+        if archivo:
+            msm=f"Archivo seleccionado: {archivo}"
+            self.archivo_enc.setText(msm)
+        else:
+            msm="No se seleccionó ningún archivo"
+            self.archivo_enc.setText(msm)
+        
+
 
 class ecg(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         loadUi('ECG.ui',self)
+        self._ventana_menu=parent
         self.setup()
     def setup(self):
+        self.volver_bot.clicked.connect(self.opcion_volver)
+        self.subir_arc.clicked.connect(self.Abrir_archivo)
+    def opcion_volver(self):
+        self.hide()
+        self._ventana_menu.show()
+    def Abrir_archivo(self):
+        archivo, _ = QFileDialog.getOpenFileName(self, "Abrir archivo", "", "Todos los archivos (*)")
+        if archivo:
+            msm=f"Archivo seleccionado: {archivo}"
+            self.archivo_enc.setText(msm)
+        else:
+            msm="No se seleccionó ningún archivo"
+            self.archivo_enc.setText(msm)
+
          
 class ppg(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        loadUi('',self)
+        loadUi('PPG.ui',self)
+        self._ventana_menu=parent
         self.setup()
     def setup(self):
+        self.volver_bot.clicked.connect(self.opcion_volver)
+        self.subir_arc.clicked.connect(self.Abrir_archivo)
+    def opcion_volver(self):
+        self.hide()
+        self._ventana_menu.show()
+    def Abrir_archivo(self):
+        archivo, _ = QFileDialog.getOpenFileName(self, "Abrir archivo", "", "Todos los archivos (*)")
+        if archivo:
+            msm=f"Archivo seleccionado: {archivo}"
+            self.archivo_enc.setText(msm)
+        else:
+            msm="No se seleccionó ningún archivo"
+            self.archivo_enc.setText(msm)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ventana=pprincipal()
